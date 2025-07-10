c3acaee90c768dd00dff3ffbdd3aee56974af164
Liubawa <1iubov.valerianovna@gmail.com>
2025-07-10 20:41:12 +0300

    Chapter 7 - Configurating Multiple Browsers

--- pages/search.py
+++ pages/search.py
@@ -1,18 +1,26 @@
 #This Module contains the Page Object for search page
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class DuckDuckGoSearchPage:
 
+    # URL
+    URL = 'https://duckduckgo.com'
+
+    # Locators
     SEARCH_INPUT = (By.ID, 'searchbox_input')
 
+    # Initializer
     def __init__(self, browser):
         #instance variable initialization
         self.browser = browser
 
+    # Interaction Methods
+
     def load(self):
-        #TODO
-        pass
+        self.browser.get(self.URL)
+
     def search(self, phase):
-        #TODO
-        pass
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
