cf040000ebbddc4e0ad9dc5d93aaa4791f35e0ef
Liubawa <1iubov.valerianovna@gmail.com>
2025-07-10 16:56:49 +0300

    Add locators for elements

--- pages/search.py
+++ pages/search.py
@@ -1,6 +1,11 @@
 #This Module contains the Page Object for search page
 
+from selenium.webdriver.common.by import By
+
 class DuckDuckGoSearchPage:
+
+    SEARCH_INPUT = (By.ID, 'searchbox_input')
+
     def __init__(self, browser):
         #instance variable initialization
         self.browser = browser
